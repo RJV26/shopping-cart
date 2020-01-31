c2efb66455a7deba10c50c3144b73801853c22b0
RJV26 <rajivyadav417@gmail.com>
2020-01-31 11:31:24 +0530

    Updates

---
+++ precache-manifest.7f93ec17c72b552e5afb722f14b9abfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2e5295e5593cc583da133a75876ab1",
+    "url": "/github.io/shopping-cart/index.html"
+  },
+  {
+    "revision": "be9cff4edf6b58d30ad6",
+    "url": "/github.io/shopping-cart/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a5573e7f912d091e6454",
+    "url": "/github.io/shopping-cart/static/css/main.de64695c.chunk.css"
+  },
+  {
+    "revision": "be9cff4edf6b58d30ad6",
+    "url": "/github.io/shopping-cart/static/js/2.27311815.chunk.js"
+  },
+  {
+    "revision": "a5573e7f912d091e6454",
+    "url": "/github.io/shopping-cart/static/js/main.7a490e7e.chunk.js"
+  },
+  {
+    "revision": "5177ecc11392bcb1642a",
+    "url": "/github.io/shopping-cart/static/js/runtime-main.dfe14081.js"
+  }
+]);
